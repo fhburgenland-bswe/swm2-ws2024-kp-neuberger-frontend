dbd8a7b7bc3e59ab1cc8c3375a0c2432b8287cbd
Der wahre Luki <159880557+Lukas-Neuberger@users.noreply.github.com>
2025-04-25 02:50:05 +0200

    feat: add book details and change evaluation
    closes #6

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,14 @@ import {NgModule} from '@angular/core';
 import {HomeComponent} from './pages/home/home.component';
 import {UserListComponent} from './pages/user-list/user-list.component';
 import {UserDetailComponent} from './pages/user-detail/user-detail.component';
+import {BookDetailComponent} from './pages/book-detail/book-detail.component';
 
 export const routes = [
   { path: '', component: HomeComponent },
   { path: 'users/create', component: UserCreateComponent },
   { path: 'users',        component: UserListComponent },
   { path: 'users/:userId',component: UserDetailComponent, data: { renderMode: 'default' },},
+  { path: 'users/:userId/books/:isbn', component: BookDetailComponent },
   { path: '**', redirectTo: '' }
 ];
 
